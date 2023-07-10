35b9a014ba06f46bdbb95f0d242741b8e2d580a5
matigimenez31 <matiasgimenez3101@gmail.com>
2023-07-10 15:06:53 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://d2r9epyceweg5n.cloudfront.net/stores/001/074/983/products/b433b600-5625aa2e-c4da-4845-b22c-5da2c44926c51-8fb31b1b3dcc0b609916813247642468-480-0.webp',
+		nameProduct: 'Nike Air Jordan 1 Low Paris',
+		price: 450,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://d2r9epyceweg5n.cloudfront.net/stores/001/074/983/products/61pzjecsrvl-_ac_ux395_-07bec94584d41f000816450353833579-480-0.jpg',
+		nameProduct: 'Nike Air Jordan Retro 4 Black Cat',
+		price: 453,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://d2r9epyceweg5n.cloudfront.net/stores/001/074/983/products/de40cb1a-1200x120011-710f320b48808a0e0616815769354363-480-0.webp',
+		nameProduct: 'Adidas Forum Low Bad Bunny Grey',
+		price: 151,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://d2r9epyceweg5n.cloudfront.net/stores/001/074/983/products/travis-scott-air-jordan-1-low-black-phantom-dm7866-001-lateral1-fde78ba52ced25069f16693049111729-480-0.jpeg',
+		nameProduct: 'Nike Jordan 1 Low x Travis Scott Black',
+		price: 112,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://d2r9epyceweg5n.cloudfront.net/stores/001/074/983/products/up-tempo-11-8ee62fce710a05723e16125501688891-480-0.jpg',
+		nameProduct: 'Nike Up Tempo Black & White',
+		price: 155,
+		quantity: 1,
+	},
+    {
+        id: 5,
+		img: 'https://d2r9epyceweg5n.cloudfront.net/stores/001/074/983/products/air-max-97-mens-shoe-sd3zmw1-b60a764e7a7cc081e015884450714649-480-0.jpg',
+		nameProduct: 'Nike Air Max 97 Black & White',
+		price: 107,
+		quantity: 1,
+    }
+
+];
